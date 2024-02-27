86118b3848b9567b654f6521ef4ed1492e00743e
Ben Chacko <bchacko@vmware.com>
2024-02-27 13:12:58 +0000

    add publish script

---
+++ publish.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e 
+
+if [ ! -v AWS_REGION ] && [ -z $1 ]; then 
+  echo "Missing required argument!"
+  exit 1
+fi
+
+check_tooling () {
+  if [ ! command -v techdocs-cli &> /dev/null ]; then
+    echo "Unable to continue... Please install techdocs-cli."
+    exit 1
+  fi
+}
+
+generate () {
+  techdocs-cli generate --no-docker --output-dir ./site
+}
+
+publish () {
+  techdocs-cli publish 	--publisher-type awsS3 \
+   			--awsEndpoint https://minio-api.skynetsystems.io \
+			--storage-name techdocs \
+			--entity default/Component/onboarding \
+			--awsS3ForcePathStyle \
+			--directory ./site
+}
+
+init () {
+  check_tooling
+  generate
+  publish
+}
+
+init
